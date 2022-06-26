da033893f57a6ccb3444a43e7e1e83f224316fc9
Mykhailo Syritchenko <syritchenko@gmail.com>
2022-06-26 13:20:54 +0200

    Modified src/App.tsx added Header

--- src/App.tsx
+++ src/App.tsx
@@ -3,41 +3,45 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 // import MenuList from './components/MenuList/MenuList';
 import UserItem from './components/UserItem/UserItem';
+import Header from './components/Header/Header';
 // import './App.css';
 
 const App = () => {
 	return (
-		<Routes>
-			<Route path="/" element={<Home />}>
-				<Route path=":userId" element={<UserItem />}>
-					<Route
-						index
-						element={
-							<main style={{ padding: '1rem' }}>
-								<p>Select an invoice</p>
-							</main>
-						}
-					/>
+		<>
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />}>
+					<Route path=":userId" element={<UserItem />}>
+						<Route
+							index
+							element={
+								<main style={{ padding: '1rem' }}>
+									<p>Select an invoice</p>
+								</main>
+							}
+						/>
+					</Route>
 				</Route>
-			</Route>
-			<Route
-				path="*"
-				element={
-					<main
-						style={{
-							display: 'flex',
-							justifyContent: 'center',
-							alignItems: 'center',
-							width: '100%',
-							minHeight: '40rem'
-						}}
-						// className="nothingClass"
-					>
-						<p>There's nothing here!</p>
-					</main>
-				}
-			/>
-		</Routes>
+				<Route
+					path="*"
+					element={
+						<main
+							style={{
+								display: 'flex',
+								justifyContent: 'center',
+								alignItems: 'center',
+								width: '100%',
+								minHeight: '40rem'
+							}}
+							// className="nothingClass"
+						>
+							<p>There's nothing here!</p>
+						</main>
+					}
+				/>
+			</Routes>
+		</>
 	);
 };
 
