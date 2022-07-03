db750fc91bdab7f07402525f755b436ca425f223
Mykhailo Syritchenko <syritchenko@gmail.com>
2022-07-04 00:13:14 +0200

    Modified Home page cmponents added Header component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,12 +3,14 @@ import { Outlet } from 'react-router-dom';
 import { Grid, Container } from '@mui/material';
 
 import MenuList from '../../components/MenuList/MenuList';
+import Header from '../../components/Header/Header';
 // import UserItem from '../../components/UserItem/UserItem';
 
 const Home: FC = () => {
 	return (
 		<Grid container spacing={7}>
 			<Grid item xs={12} sm={12} md={12} lg={12}>
+				<Header />
 				<Container
 					sx={{ display: 'flex', margin: '0', padding: '0', paddingLeft: '0' }}>
 					<MenuList />
