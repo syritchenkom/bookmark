e01cfbc03e73db2218870b0dfc0ea1190794b746
Mykhailo Syritchenko <syritchenko@gmail.com>
2022-07-04 00:07:10 +0200

    Modified src/redux/store.ts added new fnctionality

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit'
-import addBookmark from './addBookmark/bookmarkSlice';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import folder from './folder/slice'
+import bookmark from './bookmarks/slice';
 
 export const store = configureStore({
   reducer: {
-    
+    // bookmarkStore: bookmarkReducer
+    folder,
+    bookmark
   },
-})
+});
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector; //?????
