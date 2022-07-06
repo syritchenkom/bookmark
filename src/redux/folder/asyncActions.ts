8657c3e932f112be0cc69d47a5246844ed5bc0cb
Mykhailo Syritchenko <syritchenko@gmail.com>
2022-07-07 00:22:51 +0200

    Modified redux/folder/asyncActions.ts

--- src/redux/folder/asyncActions.ts
+++ src/redux/folder/asyncActions.ts
@@ -5,6 +5,6 @@ import { Folder } from "./types";
 
 export const fetchFolders = createAsyncThunk<Folder[]>('folder/fetchFolderStatus', async (params) => {
 	const { data } = await axios.get<Folder[]>('https://jsonplaceholder.typicode.com/users');
-	console.log("data", data)
+	// console.log("data", data)
 	return data;
 });
