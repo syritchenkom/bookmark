68a2534e0dda18312dbdc15a67a8dfc202a8d74f
Mykhailo Syritchenko <syritchenko@gmail.com>
2022-07-07 00:16:41 +0200

    Modified UserItem.tsx added new functionality

--- src/components/UserItem/UserItem.tsx
+++ src/components/UserItem/UserItem.tsx
@@ -1,5 +1,4 @@
 import React, { FC, useEffect, useState } from 'react';
-import { UserItemProps } from '../../types/types';
 import {
 	Table,
 	TableBody,
@@ -13,42 +12,26 @@ import {
 } from '@mui/material';
 import MoreIcon from '@mui/icons-material/MoreVert';
 import DriveFileIcon from '@mui/icons-material/InsertDriveFileOutlined';
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../redux/store';
 
-// interface UserItemProps {
-// 	posts: IPost[];
-// }
+import { useParams } from 'react-router-dom';
+
+import { useAppDispatch, useAppSelector } from '../../redux/store';
+import { fetchBookmarks } from '../../redux/bookmark/asyncActions';
 
 const UserItem: FC = () => {
-	// const bookmark = useSelector(se)
-	// const dispatch = useDispatch();
-	const newBookmark = useSelector((state: RootState) => state.bookmark);
-	console.log('newBookmarks', newBookmark);
-
-	const [bookmarks, setBookmarks] = useState<UserItemProps[]>([]);
 	const [bookMenu, setBookMenu] = useState<null | HTMLElement>(null);
 
 	// const params = useParams();
 	// const userId = params.userId
 	const { userId } = useParams();
 
-	useEffect(() => {
-		fetchUserItem();
-	}, []); //}, []);
+	const dispatch = useAppDispatch();
+	const bookmarks = useAppSelector(({ bookmark }) => bookmark.bookmarks);
+	// const newBookmark = useSelector((state: RootState) => state.bookmark);
 
-	async function fetchUserItem() {
-		try {
-			const response = await axios.get(
-				`https://jsonplaceholder.typicode.com/users/${userId}/posts`
-			);
-			setBookmarks(response.data);
-		} catch (error) {
-			alert(error);
-		}
-	}
+	useEffect(() => {
+		dispatch(fetchBookmarks({ userId }));
+	}, [dispatch, userId]);
 
 	const open = Boolean(bookMenu);
 
@@ -70,106 +53,110 @@ const UserItem: FC = () => {
 				}}>
 				<TableBody>
 					{/* Data from jsonplaceholder.typicode.com */}
-					{/* {newBookmark} */}
-					{bookmarks.map((bookmark) => (
-						<TableRow
-							hover
-							key={bookmark.id}
-							sx={{
-								display: 'flex',
-								// alignItems: 'center',
-								'&:last-child td, &:last-child th': { border: 0 }
-							}}>
-							<TableCell
+					{console.log('bookmarks', bookmarks)}
+					{bookmarks ? (
+						bookmarks.map((bookmark) => (
+							<TableRow
+								hover
+								key={bookmark.id}
 								sx={{
 									display: 'flex',
-									alignItems: 'center'
-								}}
-								component="th"
-								scope="row">
-								<DriveFileIcon sx={{ marginRight: '1rem' }} />
-								{bookmark.title}
-								{bookmark.body}
-							</TableCell>
-							<TableCell sx={{ flexGrow: 1 }} />
-							<TableCell
-								sx={{
-									// display: { xs: 'flex' }
-									display: 'flex'
+									// alignItems: 'center',
+									'&:last-child td, &:last-child th': { border: 0 }
 								}}>
-								<IconButton
-									size="small"
-									id="menu-button"
-									title="Menu Item"
-									aria-label="more"
-									aria-controls={open ? 'menu-bookmark' : undefined}
-									aria-expanded={open ? 'true' : undefined}
-									aria-haspopup="true"
-									onClick={handleBookClick}
-									// color="inherit"
-								>
-									<MoreIcon />
-								</IconButton>
-								<Menu
-									id="menu-bookmark"
-									// MenuListProps={{ 'aria-labelledby': 'menu-button' }}
-									anchorOrigin={{
-										vertical: 'top',
-										horizontal: 'right'
+								<TableCell
+									sx={{
+										display: 'flex',
+										alignItems: 'center'
 									}}
-									transformOrigin={{
-										vertical: 'top',
-										horizontal: 'right'
-									}}
-									anchorEl={bookMenu}
-									open={open}
-									onClose={handleBookClose}>
-									<MenuItem
-									// onClick={handleProfileMenuOpen}
+									component="th"
+									scope="row">
+									<DriveFileIcon sx={{ marginRight: '1rem' }} />
+									{bookmark.title}
+									{bookmark.body}
+								</TableCell>
+								<TableCell sx={{ flexGrow: 1 }} />
+								<TableCell
+									sx={{
+										// display: { xs: 'flex' }
+										display: 'flex'
+									}}>
+									<IconButton
+										size="small"
+										id="menu-button"
+										title="Menu Item"
+										aria-label="more"
+										aria-controls={open ? 'menu-bookmark' : undefined}
+										aria-expanded={open ? 'true' : undefined}
+										aria-haspopup="true"
+										onClick={handleBookClick}
+										// color="inherit"
 									>
-										Rename
-									</MenuItem>
-									<MenuItem
-										divider
+										<MoreIcon />
+									</IconButton>
+									<Menu
+										id="menu-bookmark"
+										// MenuListProps={{ 'aria-labelledby': 'menu-button' }}
+										anchorOrigin={{
+											vertical: 'top',
+											horizontal: 'right'
+										}}
+										transformOrigin={{
+											vertical: 'top',
+											horizontal: 'right'
+										}}
+										anchorEl={bookMenu}
+										open={open}
+										onClose={handleBookClose}>
+										<MenuItem
 										// onClick={handleProfileMenuOpen}
-									>
-										Delete
-									</MenuItem>
-									<MenuItem
-									// onClick={handleProfileMenuOpen}
-									>
-										Cut
-									</MenuItem>
-									<MenuItem
-									// onClick={handleProfileMenuOpen}
-									>
-										Copy
-									</MenuItem>
-									<MenuItem
-										divider
+										>
+											Rename
+										</MenuItem>
+										<MenuItem
+											divider
+											// onClick={handleProfileMenuOpen}
+										>
+											Delete
+										</MenuItem>
+										<MenuItem
 										// onClick={handleProfileMenuOpen}
-									>
-										Paste
-									</MenuItem>
-									<MenuItem
-									// onClick={handleProfileMenuOpen}
-									>
-										Open in new tab
-									</MenuItem>
-									<MenuItem
-									// onClick={handleProfileMenuOpen}
-									>
-										Open in new window
-									</MenuItem>
-									<MenuItem
-									// onClick={handleProfileMenuOpen}
-									>
-										Open in Incognito window
-									</MenuItem>
-								</Menu>
-							</TableCell>
-						</TableRow>
-					))}
+										>
+											Cut
+										</MenuItem>
+										<MenuItem
+										// onClick={handleProfileMenuOpen}
+										>
+											Copy
+										</MenuItem>
+										<MenuItem
+											divider
+											// onClick={handleProfileMenuOpen}
+										>
+											Paste
+										</MenuItem>
+										<MenuItem
+										// onClick={handleProfileMenuOpen}
+										>
+											Open in new tab
+										</MenuItem>
+										<MenuItem
+										// onClick={handleProfileMenuOpen}
+										>
+											Open in new window
+										</MenuItem>
+										<MenuItem
+										// onClick={handleProfileMenuOpen}
+										>
+											Open in Incognito window
+										</MenuItem>
+									</Menu>
+								</TableCell>
+							</TableRow>
+						))
+					) : (
+						<div>Could not find newBookmarks</div>
+					)}
 				</TableBody>
 			</Table>
 		</TableContainer>
