b57896d4780477a0cee807d56efa20a986d74020
Mykhailo Syritchenko <syritchenko@gmail.com>
2022-08-07 23:53:02 +0200

    Added new component and functionality

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+	return (
+		<div className="not-found">
+			<h1 className="display-1 mr-3"> Page NotFound</h1>
+		</div>
+	);
+};
+
+export default NotFound;
