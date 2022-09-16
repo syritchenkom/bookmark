41b3a7d53c1d5a82e3325f17d9417b86bfde51e2
Mykhailo Syritchenko <syritchenko@gmail.com>
2022-09-16 17:05:57 +0200

    Deleted comments

--- src/index.tsx
+++ src/index.tsx
@@ -10,24 +10,16 @@ const rootElement = document.getElementById('root');
 
 if (rootElement) {
 	ReactDOM.render(
-		<Provider store={store}>
+		<React.StrictMode>
 			<BrowserRouter>
-				<App />
+				<Provider store={store}>
+					<App />
+				</Provider>
 			</BrowserRouter>
-		</Provider>,
+		</React.StrictMode>,
 		rootElement
 	);
 }
-
-// ReactDOM.render(
-// 	<Provider store={store}>
-// 		<BrowserRouter>
-// 			<App />
-// 		</BrowserRouter>
-// 	</Provider>,
-// 	document.getElementById('root')
-// );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
