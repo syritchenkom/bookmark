4f0e5741dfa6caff6fc4afd33fe3fda4d95f2cbb
Mykhailo Syritchenko <syritchenko@gmail.com>
2022-09-16 17:19:26 +0200

    Renamed component

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import Header from '../Header/Header';
-import MenuList from '../MenuList/MenuList';
+import MenuList from '../FolderList/FolderList';
 
 const MainLayout: FC = () => {
 	return (
