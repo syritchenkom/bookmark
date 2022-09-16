c530c30ca36d8e0a1b7e64792a46cdc65f873c49
Mykhailo Syritchenko <syritchenko@gmail.com>
2022-09-16 15:28:09 +0200

    Cleared the code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
-import UserItem from './components/UserItem/UserItem';
+import UserItem from './components/BookmarkList/UserItem';
 // import MainLayout from './components/layouts/MainLayout';
 
 const App = () => {
 	return (
+		// https://codesandbox.io/s/cool-rubin-vwsp36?file=/src/App.js => Link to route
 		<Routes>
-			{/* https://codesandbox.io/s/cool-rubin-vwsp36?file=/src/App.js => Link to route*/}
 			<Route path="/" element={<Home />}>
-				<Route path=":userId" element={<UserItem />}>
-					<Route
-						index
-						element={
-							<div style={{ padding: '1rem' }}>
-								<p>Select an invoice</p>
-							</div>
-						}
-					/>
-				</Route>
+				<Route
+					index
+					element={
+						<div
+							style={{
+								display: 'flex',
+								justifyContent: 'center',
+								alignItems: 'center',
+								width: '100%',
+								minHeight: '40rem'
+							}}>
+							<p>
+								&lt;There's nothing here!&gt;
+								<br />
+								{'<='} Select an invoice on left
+							</p>
+						</div>
+					}
+				/>
+				<Route path=":userId" element={<UserItem />} />
+				{/* <Route path="*" element={<Navigate to="/" replace />} /> */}
 				<Route
 					path="*"
 					element={
@@ -29,9 +40,7 @@ const App = () => {
 								alignItems: 'center',
 								width: '100%',
 								minHeight: '40rem'
-							}}
-							// className="nothingClass"
-						>
+							}}>
 							<p>There's nothing here!</p>
 						</div>
 					}
