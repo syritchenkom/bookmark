28ef28e9a37eb8a1daf3d40016433fbd27c5629d
Mykhailo Syritchenko <syritchenko@gmail.com>
2022-10-01 23:34:57 +0200

    Added Skeleton to components

--- src/components/BookmarkList/Skeleton.tsx
+++ src/components/BookmarkList/Skeleton.tsx
@@ -8,8 +8,7 @@ const Skeleton = () => (
 		height={110}
 		viewBox="0 0 762 110"
 		backgroundColor="#dbdbdb"
-		foregroundColor="#bdbdbd"
-		{...props}>
+		foregroundColor="#bdbdbd">
 		<rect x="0" y="0" rx="0" ry="0" width="762" height="110" />
 	</ContentLoader>
 );
