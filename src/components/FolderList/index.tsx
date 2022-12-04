49d85a00d4a42f44e486b6e9be47647a30cc172f
Mykhailo Syritchenko <syritchenko@gmail.com>
2022-12-04 12:46:04 +0100

    Modified code, add new components, cleared and destructuring code

--- src/components/FolderList/index.tsx
+++ src/components/FolderList/index.tsx
@@ -8,21 +8,39 @@ import {
 import { Folder } from '@mui/icons-material';
 import { NavLink } from 'react-router-dom';
 
-// import { useSelector } from 'react-redux';
 import { useAppDispatch, useAppSelector } from '../../redux/store';
 import { fetchFolders } from '../../redux/folder/asyncActions';
+import { selectFolderData } from '../../redux/folder/selectors';
+//??? \/ Test \/
+import { searchBookmark } from '../../redux/bookmark/slice';
 
 const FolderList: FC = () => {
 	const [open] = useState(false);
 
 	const dispatch = useAppDispatch();
-	const folders = useAppSelector(({ folder }) => folder.folders);
+	// const folders = useAppSelector(({ folder }) => folder.folders);
+	const folders = useAppSelector(selectFolderData);
+
+	// const activeStyle = {
+	// 	display: 'block',
+	// 	margin: '1rem 0',
+	// 	color: 'black',
+	// 	// backgroundColor: isActive ? 'skyBlue' : '',
+	// 	// backgroundColor: 'skyBlue',
+	// 	backgroundColor: searchGlobalBookmark ? '' : 'skyBlue',
+	// 	borderRadius: '25px 0 0 25px',
+	// 	textDecoration: 'none'
+	// };
 
 	useEffect(() => {
 		if (!folders?.length) {
 			dispatch(fetchFolders());
 		}
 	}, [dispatch, folders?.length]);
+
+	/* let activeStyle = {
+		textDecoration: 'underline'
+	}; */
 
 	return (
 		<List
@@ -53,10 +71,9 @@ const FolderList: FC = () => {
 							};
 						}}
 						to={`/${folder.id}`}
+						// isActive={activeStyle}
 						key={folder.id}>
-						<ListItemButton
-							// sx={{ borderRadius: '25px 0 0 25px' }}
-							selected={open}>
+						<ListItemButton selected={open}>
 							<ListItemIcon sx={{ minWidth: 40 }}>
 								<Folder />
 							</ListItemIcon>
