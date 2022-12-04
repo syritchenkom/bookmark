0b2408e4835e46021bf4f9d6ca3e1dd21c84b7e6
Mykhailo Syritchenko <syritchenko@gmail.com>
2022-12-04 13:00:08 +0100

    Modified code, add new components, functionality, cleared and destructuring code

--- src/redux/bookmark/asyncActions.ts
+++ src/redux/bookmark/asyncActions.ts
@@ -1,34 +1,54 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-import { Bookmark} from "./types"
+import { filterFolders } from "../folder/slice";
+import { Bookmark } from "./types";
 
- 
-export const fetchBookmarks = createAsyncThunk('bookmark/fetchBookmarkStatus', async (params: { userId: string | undefined} ) => {
+
+
+export const fetchBookmarks = createAsyncThunk('bookmark/fetchBookmarkStatus', async (params: { userId: string | undefined}, thunkAPI ) => {
+	try {
 	const {userId}  = params;
-	// console.log("userId", userId)
+	// console.log(thunkAPI.getState())
 	const {data} = await axios.get<Bookmark[]>(
 				`https://jsonplaceholder.typicode.com/users/${userId}/posts`
 			);
 	return data;
+	} catch (error: any){ // ?????????????
+		// return thunkAPI.rejectWithValue('Something went wrong...');
+		return thunkAPI.rejectWithValue(error.message);
+	}
 });
 
-export const renameBookmark = createAsyncThunk('bookmark/renameBookmark', async (params: {userId: string | undefined}) => {
+export const renameBookmark = createAsyncThunk('bookmark/renameBookmarkStatus', async (params: {userId: string | undefined}) => {
 	const {userId} = params;
 	try {
 		const response = await axios.put<Bookmark[]>(`https://jsonplaceholder.typicode.com/users/${userId}/posts`, params
 		)
 		return response.data
-	}catch(err){
+	}catch(error){
 		//return err.message;
 		return params; //only for testing Redux!
 	}
-})
+});
 
-export const deleteBookmark = createAsyncThunk('bookmark/deleteBookmark', async (params: {userId: number | undefined}) => {
+export const deleteBookmark = createAsyncThunk('bookmark/deleteBookmarkStatus', async (params: {userId: number | undefined}) => {
 	const {userId} = params;
 	// console.log("userId", userId)
 	const {data} = await axios.delete<Bookmark[]>(
 				`https://jsonplaceholder.typicode.com/users/${userId}/posts`
 			);
 	return data;
-})
+});
+
+export const searchGlobalBookmark = createAsyncThunk('bookmark/searchGlobalBookmarkStatus', async(title: string, thunkAPI) => {
+	const {data} = await axios.get<Bookmark[]>(
+		`https://jsonplaceholder.typicode.com/posts`
+	);
+	const value = title.toLowerCase();
+			console.log('value', value);
+			const bookmarks = data.filter((el) => el.title.toLowerCase().includes(value) || el.body.toLowerCase().includes(value));
+			const userIds = bookmarks.map(bookmark => bookmark.userId)
+			thunkAPI.dispatch(filterFolders(userIds))
+
+	return bookmarks;
+});
