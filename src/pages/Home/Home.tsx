143184e69f31592861c68fbc67c72a035df4c8d6
Mykhailo Syritchenko <syritchenko@gmail.com>
2023-01-10 02:10:35 +0100

    Added CssBaseline function, ThemeProvider styles, useAppSelector and import darkTheme and lightTheme

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,23 +1,37 @@
 import React, { FC } from 'react';
 import { Outlet } from 'react-router-dom';
-import { Grid, Container } from '@mui/material';
+import { Grid, Container, CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
 
 import FolderList from '../../components/FolderList';
 import Header from '../../components/Header';
-// import UserItem from '../../components/UserItem/UserItem';
+
+import { useAppSelector } from '../../redux/store';
+import { darkTheme, lightTheme } from '../../components/Theme/Theme';
+
 
 const Home: FC = () => {
+	// get theme from store
+	const theme = useAppSelector(({ theme }) => theme.darkTheme);
+
 	return (
-		<Grid container spacing={7}>
-			<Grid item xs={12} sm={12} md={12} lg={12}>
-				<Header />
-				<Container
-					sx={{ display: 'flex', margin: '0', padding: '0', paddingLeft: '0' }}>
-					<FolderList />
-					<Outlet />
-				</Container>
+		<ThemeProvider theme={theme ? darkTheme : lightTheme}>
+			<CssBaseline />
+			<Grid container spacing={0}>
+				<Grid item xs={12} sm={12} md={12} lg={12} sx={{ paddingLeft: '0' }}>
+					<Header />
+					<Container
+						sx={{
+							display: 'flex',
+							margin: '0',
+							padding: '0'
+						}}>
+						<FolderList />
+						<Outlet />
+					</Container>
+				</Grid>
 			</Grid>
-		</Grid>
+		</ThemeProvider>
 	);
 };
 
