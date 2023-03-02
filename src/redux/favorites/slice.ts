7a69ac5a69f739859fb5785c3ee98c2dd8da4cce
syritchenkom <syritchenko@gmail.com>
2023-03-02 12:02:42 +0100

    Created favoriteSlice component and addToFavorites, removeFromFavorites reducers

--- src/redux/favorites/slice.ts
+++ src/redux/favorites/slice.ts
@@ -5,19 +5,22 @@ const initialState: FavoritesState = {
    favorites: [],
 };
 
-const favoritesSlice = createSlice({
+export const favoritesSlice = createSlice({
    name: "favorites",
    initialState,
    reducers: {
       addToFavorites: (state, action: PayloadAction<Favorite>) => {
+         console.log("state", state)
+         console.log("action", action)
          state.favorites.push(action.payload)
       },
       removeFromFavorites: (state, action: PayloadAction<Favorite>) => {
-         state.favorites = state.favorites.filter(favorite => favorite.id !== action.payload.id)
+         const bookId = action.payload;
+         state.favorites = state.favorites.filter(favorite => favorite.id !== bookId.id)
       }
    }
 })
 
 export const {addToFavorites, removeFromFavorites} = favoritesSlice.actions;
 
-export default favoritesSlice;
+export default favoritesSlice.reducer;
