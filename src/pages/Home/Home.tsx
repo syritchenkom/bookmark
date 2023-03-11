00d40fba3a5c2dd0787f6f4b355aeb9f56407d36
syritchenkom <syritchenko@gmail.com>
2023-03-11 17:34:08 +0100

    Upload code

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -11,28 +11,28 @@ import { darkTheme, lightTheme } from '../../components/Theme/Theme';
 
 
 const Home: FC = () => {
-	// get theme from store
-	const theme = useAppSelector(({ theme }) => theme.darkTheme);
+  // get theme from store
+  const theme = useAppSelector(({ theme }) => theme.darkTheme);
 
-	return (
-		<ThemeProvider theme={theme ? darkTheme : lightTheme}>
-			<CssBaseline />
-			<Grid container spacing={0}>
-				<Grid item xs={12} sm={12} md={12} lg={12} sx={{ paddingLeft: '0' }}>
-					<Header />
-					<Container
-						sx={{
-							display: 'flex',
-							margin: '0',
-							padding: '0'
-						}}>
-						<FolderList />
-						<Outlet />
-					</Container>
-				</Grid>
-			</Grid>
-		</ThemeProvider>
-	);
+  return (
+    <ThemeProvider theme={theme ? darkTheme : lightTheme}>
+      <CssBaseline />
+      <Grid container spacing={0}>
+        <Grid item xs={12} sm={12} md={12} lg={12} sx={{ paddingLeft: '0' }}>
+          <Header />
+          <Container
+            sx={{
+              display: 'flex',
+              margin: '0',
+              padding: '0'
+            }}>
+            <FolderList />
+            <Outlet />
+          </Container>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
 };
 
 export default Home;
