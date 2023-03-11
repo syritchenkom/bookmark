3498148577b21b6360162fca171c30db8f1b4bc5
syritchenkom <syritchenko@gmail.com>
2023-03-11 17:36:32 +0100

    Added reducers toggleActiveFavorites and functionality

--- src/redux/favorite/slice.ts
+++ src/redux/favorite/slice.ts
@@ -1,8 +1,11 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import {Favorite, FavoritesState} from './types';
 
+
+
 const initialState: FavoritesState = {
    favorites: [],
+   isActive: false,
 };
 
 export const favoritesSlice = createSlice({
@@ -10,17 +13,25 @@ export const favoritesSlice = createSlice({
    initialState,
    reducers: {
       addToFavorites: (state, action: PayloadAction<Favorite>) => {
-         console.log("action", action)
+         // console.log("action", action)
          const bookId = action.payload;
          state.favorites.push(bookId)
       },
       removeFromFavorites: (state, action: PayloadAction<Favorite>) => {
          const bookId = action.payload;
          state.favorites = state.favorites.filter(favorite => favorite.id !== bookId.id)
+      },
+      toggleActiveFavorite: (state) => {
+         // console.log("isActive", state.isActive)
+         // console.log("isActive.payload", action.payload)
+         // state.isActive = action.payload;
+         state.isActive = !state.isActive;
+         console.log("isActive_reducers", !state.isActive)
+
       }
    }
 })
 
-export const {addToFavorites, removeFromFavorites} = favoritesSlice.actions;
+export const {addToFavorites, removeFromFavorites, toggleActiveFavorite} = favoritesSlice.actions;
 
 export default favoritesSlice.reducer;
