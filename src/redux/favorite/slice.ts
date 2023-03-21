32a28894e9333bf6b5df9b01a461af772263bf18
syritchenkom <syritchenko@gmail.com>
2023-03-21 11:11:18 +0100

    Added active status, and setActive functionality

--- src/redux/favorite/slice.ts
+++ src/redux/favorite/slice.ts
@@ -5,7 +5,7 @@ import {Favorite, FavoritesState} from './types';
 
 const initialState: FavoritesState = {
    favorites: [],
-   isActive: false,
+   active: false,
 };
 
 export const favoritesSlice = createSlice({
@@ -13,25 +13,50 @@ export const favoritesSlice = createSlice({
    initialState,
    reducers: {
       addToFavorites: (state, action: PayloadAction<Favorite>) => {
-         // console.log("action", action)
          const bookId = action.payload;
          state.favorites.push(bookId)
+        //  console.log("bookId", bookId)
+        //  const index = state.favorites.indexOf(bookId);
+        //  console.log("indexFavorite", index)
+        //  console.log("stateFavorite", state.favorites)
+        //  console.log("actionFavorite", state.favorites.push(action.payload))
+
+        //  if(index === -1){
+        //     state.favorites.push(bookId)
+        //  }else{
+        //     state.favorites.splice(index,1);
+        //  }
       },
       removeFromFavorites: (state, action: PayloadAction<Favorite>) => {
          const bookId = action.payload;
          state.favorites = state.favorites.filter(favorite => favorite.id !== bookId.id)
       },
-      toggleActiveFavorite: (state) => {
-         // console.log("isActive", state.isActive)
-         // console.log("isActive.payload", action.payload)
-         // state.isActive = action.payload;
-         state.isActive = !state.isActive;
-         console.log("isActive_reducers", !state.isActive)
-
+      setActive: (state, action: PayloadAction<boolean>) => {
+         state.active = action.payload;
       }
+      // addSelectedBookmarkId(state, action: PayloadAction<string>){
+      //    state.selectedBookmarkIds.push(action.payload);
+      // }
+      // toggleFavorite: (state, action: PayloadAction<FavoritesState>) => {
+      //    // console.log("isActive", state.isActive)
+      //    // console.log("isActive.payload", action.payload)
+      //    // state.isActive = !state.isActive;
+            // const index = state.selected.indexOf(action.payload);
+         //  if(index === -1){
+            //  state.selected.push(action.payload);
+         //  }else{
+            //  state.selected.splice(index, 1);
+         //  }
+      //    const bookIndex = state.favorites.findIndex((bookmark) => bookmark.id === action.payload);
+      //    if(bookIndex !== -1) {
+      //       state.favorites.selected = !state.favorites.selected;
+      //    }
+      //    console.log("isFavorite_bookIndex_reducers", bookIndex)
+
+      // }
    }
 })
 
-export const {addToFavorites, removeFromFavorites, toggleActiveFavorite} = favoritesSlice.actions;
+export const {addToFavorites, removeFromFavorites, setActive } = favoritesSlice.actions;
 
 export default favoritesSlice.reducer;
