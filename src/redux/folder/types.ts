d9471043ace0d17b43912a1b54d41a93526a34ba
syritchenkom <syritchenko@gmail.com>
2023-04-18 20:58:58 +0200

    Deleting one type from typeFolder and cleaned code

--- src/redux/folder/types.ts
+++ src/redux/folder/types.ts
@@ -1,5 +1,4 @@
 export type Folder = {
-  [x: string]: any;
 	id: number;
 	name: string;
 	username: string;
@@ -36,9 +35,8 @@ export enum Status {
 }
 
 export interface FolderSliceState {
-   	folders: Folder[];
+  folders: Folder[];
 	filterFolders: Folder[];
-	// sortBy: string;
-   	status: Status;
+  status: Status;
 	searchGlobal: boolean;
 }
