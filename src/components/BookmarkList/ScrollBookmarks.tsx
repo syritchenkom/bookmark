8b54e4d3305842333bb641f59c902478cf5e5fc0
syritchenkom <syritchenko@gmail.com>
2023-05-24 00:27:16 +0200

    Added and changed functioonals in the components

--- src/components/BookmarkList/ScrollBookmarks.tsx
+++ src/components/BookmarkList/ScrollBookmarks.tsx
@@ -5,54 +5,53 @@ import { updateScrollBookmark } from '../../redux/bookmark/asyncActions';
 import { Box, Fab } from '@mui/material';
 
 const useStyles = {
-	toTop: {
-		zIndex: 2,
-		position: 'fixed',
-		bottom: '2vh',
-		backgroundColor: '#DCDCDC',
-		color: 'black',
-		'&:hover, &.Mui-focusVisible': {
-			transition: '0.3s',
-			color: '#397BA6',
-			backgroundColor: '#DCDCDC'
-		},
-		right: '5%'
-	},
-	toBottom: {
-		zIndex: 2,
-		position: 'fixed',
-		bottom: '2vh',
-		backgroundColor: '#DCDCDC',
-		color: 'black',
-		'&:hover, &.Mui-focusVisible': {
-			transition: '0.3s',
-			color: '#397BA6',
-			backgroundColor: '#DCDCDC'
-		},
-		right: '5%'
-	}
+  toTop: {
+    zIndex: 2,
+    position: 'fixed',
+    bottom: '2vh',
+    backgroundColor: '#DCDCDC',
+    color: 'black',
+    '&:hover, &.Mui-focusVisible': {
+      transition: '0.3s',
+      color: '#397BA6',
+      backgroundColor: '#DCDCDC',
+    },
+    right: '5%',
+  },
+  toBottom: {
+    zIndex: 2,
+    position: 'fixed',
+    bottom: '2vh',
+    backgroundColor: '#DCDCDC',
+    color: 'black',
+    '&:hover, &.Mui-focusVisible': {
+      transition: '0.3s',
+      color: '#397BA6',
+      backgroundColor: '#DCDCDC',
+    },
+    right: '5%',
+  },
 };
 
 export const ScrollBookmarks = () => {
-	const dispatch = useAppDispatch();
-	const scrollPosition = useAppSelector(({ bookmark }) => bookmark.position);
+  const dispatch = useAppDispatch();
+  const scrollPosition = useAppSelector(({ bookmark }) => bookmark.position);
 
-	useEffect(() => {
-		const handleScroll = () =>
-			dispatch(updateScrollBookmark(window.pageYOffset));
-		window.addEventListener('scroll', handleScroll);
-		return () => window.removeEventListener('scroll', handleScroll);
-	}, [dispatch]);
+  useEffect(() => {
+    const handleScroll = () => dispatch(updateScrollBookmark(window.pageYOffset));
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [dispatch]);
 
-	const handleClickTop = () => {
-		window.scrollTo({ top: 0, behavior: `smooth` });
-	};
+  const handleClickTop = () => {
+    window.scrollTo({ top: 0, behavior: `smooth` });
+  };
 
-	const handleClickDown = () => {
-		window.scrollTo({ top: document.body.scrollHeight, behavior: `smooth` });
-	};
+  const handleClickDown = () => {
+    window.scrollTo({ top: document.body.scrollHeight, behavior: `smooth` });
+  };
 
-	/* const handleScroll = () => {
+  /* const handleScroll = () => {
 		if (window.pageYOffset > showBelow) {
 			if (!show) setShow(true);
 		} else {
@@ -70,26 +69,18 @@ export const ScrollBookmarks = () => {
 	};
 	*/
 
-	return (
-		<Box>
-			{scrollPosition > 50 && (
-				<Fab
-					sx={useStyles.toTop}
-					color="secondary"
-					size="small"
-					onClick={handleClickTop}>
-					<KeyboardArrowUp />
-				</Fab>
-			)}
-			{scrollPosition < 50 && (
-				<Fab
-					sx={useStyles.toBottom}
-					color="secondary"
-					size="small"
-					onClick={handleClickDown}>
-					<KeyboardArrowDown />
-				</Fab>
-			)}
-		</Box>
-	);
+  return (
+    <Box>
+      {scrollPosition > 50 && (
+        <Fab sx={useStyles.toTop} color="secondary" size="small" onClick={handleClickTop}>
+          <KeyboardArrowUp />
+        </Fab>
+      )}
+      {scrollPosition < 50 && (
+        <Fab sx={useStyles.toBottom} color="secondary" size="small" onClick={handleClickDown}>
+          <KeyboardArrowDown />
+        </Fab>
+      )}
+    </Box>
+  );
 };
